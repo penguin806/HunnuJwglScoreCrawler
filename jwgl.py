a40562c8648e2d8c69859803048da1b8d211471e
Snow <polarpenguin@foxmail.com>
2020-01-13 05:09:58 +0800

    Fetch multiple courses

--- jwgl.py
+++ jwgl.py
@@ -11,15 +11,18 @@ jar.set('SERVERNAME', 'xk3')
 jar.set('GSESSIONID', '484D0BF3ACAE818F485245B722641601.node1')
 session.cookies = jar
 
-courseIdToFind = '嵌入式系统'
+courseIdToFind = ['嵌入式系统', 'Web应用系统设计']
 # request_toJWGL = session.get('http://jwglnew.hunnu.edu.cn/eams/teach/grade/course/person!search.action?semesterId=82&projectType=&_=1578409087439')
 result = session.get('http://jwglnew.hunnu.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action')
-gradeRow = result.html.find('div.grid>table.gridtable>tbody tr', containing = courseIdToFind)
+gradeRows = result.html.find('div.grid>table.gridtable>tbody tr', containing = courseIdToFind)
 
-if len(gradeRow) > 0:
-    courseId = gradeRow[0].find('td:nth-child(3)')[0].text
-    courseName = gradeRow[0].find('td:nth-child(4)')[0].text
-    courseScore = gradeRow[0].find('td:nth-last-child(3)')[0].text
-    print(courseId + ' ' + courseName + ': ' + courseScore)
+if len(gradeRows) > 0:
+    textBuffer = ''
+    for item in gradeRows:
+        courseId = item.find('td:nth-child(3)')[0].text
+        courseName = item.find('td:nth-child(4)')[0].text
+        courseScore = item.find('td:nth-last-child(3)')[0].text
+        textBuffer += courseId + ' ' + courseName + ': ' + courseScore + '\n'
+    print(textBuffer)
 else:
-    print(courseIdToFind + 'Not Found')
+    print('[' + ','.join(courseIdToFind) + '] Not Found')
