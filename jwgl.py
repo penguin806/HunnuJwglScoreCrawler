3511fe613d92fb9802927e9b3d23f99bd7af7d6a
Snow <polarpenguin@foxmail.com>
2020-01-13 04:56:54 +0800

    Fetch score of specific course implemented

--- jwgl.py
+++ jwgl.py
@@ -1,17 +1,25 @@
 import requests
+from requests_html import HTMLSession
 
-session = requests.Session()
+session = HTMLSession()
 jar = requests.cookies.RequestsCookieJar()
 
-jar.set('JSESSIONID', 'EC7D43D0C1C38A9D7216C9548BF01A72.node1')
-jar.set('C2RT', 'fb2e46ee2c322709f4d18a908e81f9c3')
+jar.set('JSESSIONID', '484D0BF3ACAE818F485245B722641601.node1')
+jar.set('C2RT', 'b435f32d05e0ff4cf87d0edf16a417e6')
 jar.set('bocms_visite_user_session', 'C816B689B1A91CC278FD5FCD7CD1CD61')
 jar.set('SERVERNAME', 'xk3')
-jar.set('GSESSIONID', 'EC7D43D0C1C38A9D7216C9548BF01A72.node1')
+jar.set('GSESSIONID', '484D0BF3ACAE818F485245B722641601.node1')
 session.cookies = jar
 
+courseIdToFind = '嵌入式系统'
 # request_toJWGL = session.get('http://jwglnew.hunnu.edu.cn/eams/teach/grade/course/person!search.action?semesterId=82&projectType=&_=1578409087439')
-request_toJWGL = session.get('http://jwglnew.hunnu.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action')
+result = session.get('http://jwglnew.hunnu.edu.cn/eams/teach/grade/course/person!historyCourseGrade.action')
+gradeRow = result.html.find('div.grid>table.gridtable>tbody tr', containing = courseIdToFind)
 
-
-print(request_toJWGL.text)
+if len(gradeRow) > 0:
+    courseId = gradeRow[0].find('td:nth-child(3)')[0].text
+    courseName = gradeRow[0].find('td:nth-child(4)')[0].text
+    courseScore = gradeRow[0].find('td:nth-last-child(3)')[0].text
+    print(courseId + ' ' + courseName + ': ' + courseScore)
+else:
+    print(courseIdToFind + 'Not Found')
